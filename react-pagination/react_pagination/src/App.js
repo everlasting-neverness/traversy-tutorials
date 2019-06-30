6938a05ba070581ddf1971d3ecea1edb76969b92
Vasily <gerasimovv1990@gmail.com>
2019-06-30 05:58:52 +0300

    update repo

---
+++ react-pagination/react_pagination/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+
+function App() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            const res = await axios.get(
+                'https://jsonplaceholder.typicode.com/posts'
+            );
+            setPosts(res.data);
+            setLoading(false);
+        };
+
+        fetchPosts();
+    }, []);
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage,
+        indexOfFirstPost = indexOfLastPost - postsPerPage,
+        currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+    return (
+        <div className='container mt-5'>
+            <h1 className='text-primary mb-3'>React Pagination</h1>
+            <Posts posts={currentPosts} loading={loading} />
+            <Pagination
+                currentPage={currentPage}
+                postsPerPage={postsPerPage}
+                totalPosts={posts.length}
+                paginate={paginate}
+            />
+        </div>
+    );
+}
+
+export default App;
